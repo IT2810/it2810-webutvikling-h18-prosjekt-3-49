1a1268c983ace3161045382770636798809bdc5f
Benjamin Fimreite <benjamfi@stud.ntnu.no>
2018-10-11 11:46:57 +0200

    Add asyncstorage-functionality to App.js #4

--- opg3/App.js
+++ opg3/App.js
@@ -1,21 +1,63 @@
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import {StyleSheet, Text, View, AsyncStorage} from 'react-native';
+import Goals from './Goals.js';
+
 
 export default class App extends React.Component {
-  render() {
-    return (
-      <View style={styles.container}>
-        <Text>Open up App.js to start working on your app!</Text>
-      </View>
-    );
-  }
+
+    constructor(props) {
+        super(props);
+        this.state = {
+            text: "Sampletext"
+        };
+        this.callBack = this.callBack.bind(this);
+    }
+
+    _storeData = async (key, text) => {
+        try {
+            await AsyncStorage.setItem(key, text);
+        } catch (error) {
+            // Error saving data
+        }
+    };
+
+    _retrieveData = async (key) => {
+        try {
+            const value = await AsyncStorage.getItem(key);
+            if (value !== null) {
+                console.log(value);
+                this.setState({text: value});
+            }
+        } catch (error) {
+            // Error retrieving data
+        }
+    };
+
+    callBack(key, text) {
+        this._storeData(key, text);
+        this._retrieveData("key");
+    }
+
+    componentDidMount() {
+        this._retrieveData("key");
+    }
+
+    render() {
+        return (
+            <View style={styles.container}>
+                <Text>{this.state.text}</Text>
+                <Goals callBack={this.callBack}/>
+            </View>
+
+        );
+    }
 }
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
+    container: {
+        flex: 1,
+        backgroundColor: '#fff',
+        alignItems: 'center',
+        justifyContent: 'center',
+    },
 });
