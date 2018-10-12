74e5e7ad8791c873cadc1d3deeec47c738af334f
Benjamin Fimreite <benjamfi@stud.ntnu.no>
2018-10-12 10:33:52 +0200

    Add more asyncstorage-functions and move them into own class #4

--- opg3/App.js
+++ opg3/App.js
@@ -1,54 +1,15 @@
 import React from 'react';
 import {StyleSheet, Text, View, AsyncStorage} from 'react-native';
 import Goals from './Goals.js';
+import Storage from './Storage';
 
 
 export default class App extends React.Component {
-
-    constructor(props) {
-        super(props);
-        this.state = {
-            text: "Sampletext"
-        };
-        this.callBack = this.callBack.bind(this);
-    }
-
-    _storeData = async (key, text) => {
-        try {
-            await AsyncStorage.setItem(key, text);
-        } catch (error) {
-            // Error saving data
-        }
-    };
-
-    _retrieveData = async (key) => {
-        try {
-            const value = await AsyncStorage.getItem(key);
-            if (value !== null) {
-                console.log(value);
-                this.setState({text: value});
-            }
-        } catch (error) {
-            // Error retrieving data
-        }
-    };
-
-    callBack(key, text) {
-        this._storeData(key, text);
-        this._retrieveData("key");
-    }
-
-    componentDidMount() {
-        this._retrieveData("key");
-    }
-
     render() {
         return (
             <View style={styles.container}>
-                <Text>{this.state.text}</Text>
-                <Goals callBack={this.callBack}/>
+                <Goals/>
             </View>
-
         );
     }
 }
--- opg3/Goals.js
+++ opg3/Goals.js
@@ -1,14 +1,35 @@
 import React from 'react';
-import {View, TextInput} from 'react-native';
+import {View, Text, TextInput} from 'react-native';
+import Storage from './Storage';
 
 export default class Goals extends React.Component {
+
+    constructor(props) {
+        super(props);
+        this.state = {
+            storage: new Storage(),
+            goals: ""
+        }
+    }
+
+    componentDidMount() {
+        this.state.storage._retrieveData("key1").then((res) => {
+            this.setState({goals: res});
+        });
+    }
+
     render() {
         return (
-            <TextInput
-                onSubmitEditing={(e) => {
-                    this.props.callBack("key", e.nativeEvent.text)
-                }}
-            />
+            <View>
+                <Text>
+                    Dine mål er: {this.state.goals}
+                </Text>
+                <TextInput
+                    onSubmitEditing={(e) => {
+                        this.state.storage._appendData("key1", e.nativeEvent.text)
+                    }}
+                />
+            </View>
         )
     }
 }
