cdfe2873ffe557e076d3a234ab8ee038f1e2b0ed
Lars <lars.moster@hotmail.com>
2018-10-15 12:07:40 +0200

    add update to contatcs

--- opg3/contacts.js
+++ opg3/contacts.js
@@ -1,13 +1,43 @@
 //[]  ()  {}  ⟨⟩
 import React, { Component} from 'react';
 import {AppRegistry, Text, View} from 'react-native'
+import Storage from "./Storage.js"
 
 
-//this.addContact = this.addContact.bind(this);
 
+
+export default class Contacts extends React.Component{
+
+    constructor(props){
+        super(props);
+        this.state={ contacts: [
+            {fname:"Lars", lname:"Møster"},
+            {fname:"Benjamin", lname:"Fimreite"},
+            {fname:"Birger", lname:"Topphol"}
+            ],
+            storage: new Storage()
+        };
+
+        this.addContact = this.addContact.bind(this);
+    }
+
+    addContact(dic) {
+        this.state.contacts.push(dic);
+        this.state.storage._storeData(
+            "contacts",
+            this.state.contacts)
+    }
+
+    render() {
+        this.addContact({fname:"Jan", lname:"Fosse"});
+        return(
+            <View>
+                <Text>
+                    Contacts: {this.state.contacts[0]}
+                </Text>
+            </View>
+        )
+    }
+}
 //contacts: list of dictionaries {fname,lname}
-let contacts = [
-    {fname:"Lars", lname:"Møster"},
-    {fname:"Benjamin", lname:"Fimreite"},
-    {fname:"Birger", lname:""}
-    ];
+
