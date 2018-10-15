686f45ab8524d2e8ba0a615f27fe2a61223ea497
Lars <lars.moster@hotmail.com>
2018-10-15 11:37:53 +0200

    revamp structure of contacts

--- opg3/contacts.js
+++ opg3/contacts.js
@@ -1,39 +1,13 @@
+//[]  ()  {}  ⟨⟩
 import React, { Component} from 'react';
 import {AppRegistry, Text, View} from 'react-native'
 
-class Contact extends Component {
-    render() {
-        return (
-            <Text>Name: {this.props.fname} {this.props.lname} </Text>
-        );
-    }
-}
 
-export default class ContactManager extends Component {
-    constructor(props) {
-        super(props);
-        this.addContact = this.addContact.bind(this);
-        this.state = {contacts : [
-            <Contact fname='Lais' lname='Umes'/>,
-            <Contact fname='Lisa' lname='Meesa'/>
-        ]
-    };
+//this.addContact = this.addContact.bind(this);
 
-    addContact(contact) {
-        this.setState((state,contact) =>
-            ({
-                contacts: state.contacts.append(contact)
-            }));
-    }
-
-    render() {
-        return (
-            this.addContact(<Contact fname='Lezz' lname='Esam' />)
-            <View>
-                <FlatList data = {this.getState.contacts}/>
-            </View>
-        );
-    }
-}
-
-AppRegistry.registerComponent('AwesomeProject', () => ContactManager);
+//contacts: list of dictionaries {fname,lname}
+let contacts = [
+    {fname:"Lars", lname:"Møster"},
+    {fname:"Benjamin", lname:"Fimreite"},
+    {fname:"Birger", lname:""}
+    ];
