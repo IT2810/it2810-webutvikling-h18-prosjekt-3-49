be997b186792964192bbd296ec2df07eac745e13
Lars <lars.moster@hotmail.com>
2018-10-18 15:03:12 +0200

    add significant functionality to contact list, involving removing contacts, object structure and buttons #14

--- opg3/Contacts.js
+++ opg3/Contacts.js
@@ -1,11 +1,33 @@
 import React, { Component} from 'react';
-import {AppRegistry, Text, View, FlatList} from 'react-native'
+import {AppRegistry, Text, View, Button} from 'react-native'
 
 
 class Contact extends Component{
+    constructor(props) {
+        super(props);
+        this.delete = this.delete.bind(this);
+        this.state = {renderMe: true};
+    }
+    delete() {
+        this.props.removeContact(this.props.id);
+        this.setState({renderMe: false});
+    }
+
     render() {
         return (
-            <Text>Name: {this.props.fname} {this.props.lname} </Text>
+            <View>
+                {this.state.renderMe &&
+                <View>
+                    <Text> {this.props.fname} {this.props.lname} </Text>
+                    <Button
+                        onPress={this.delete}
+                        title="Delete"
+                        color="#FF2018"
+                        accessibilityLabel="accessibilityLabel er denne teksten"
+                    />
+                </View>
+                }
+            </View>
         );
     }
 }
@@ -13,60 +35,88 @@ class Contact extends Component{
 export default class ContactManager extends Component {
     constructor(props) {
         super(props);
+        this.removeContact = this.removeContact.bind(this);
         this.state = {
             contacts: [
-                {fname: 'Lais', lname:'Jens'},
-                {fname: 'Lisa', lname:'Samog'}
+                <Contact
+                    removeContact={this.removeContact}
+                    id={Math.random()}
+                    fname='Lars' lname='Jens' />,
+                <Contact
+                    removeContact={this.removeContact}
+                    id={Math.random()}
+                    fname='Ask' lname='Yri' />,
             ]
         };
-
 
     }
 
     componentDidMount() {
-        this.addContact('Jens', 'Oshuendo');
-        this.removeContact('Lais', 'Jens');
+        this.addContact('Nils', 'Oshuendo');
     }
 
     addContact(fname,lname) {
-        let data = this.state.contacts;
-        data.push({fname: fname, lname: lname});
+        let contacts = this.state.contacts;
+        contacts.push(<Contact removeContact={this.removeContact}
+                           id={Math.random()}
+                           fname={fname} lname={lname} />);
         this.setState( {
-                contacts: data
+                contacts: contacts
     })}
 
-    removeContact(fname,lname) {
-        let data = this.state.contacts;
-        let index = data.indexOf({fname,lname});
-        if (index > -1) {
-            data.splice(index, 1);
+
+    removeContact(id) {
+        let contacts = this.state.contacts;
+
+        contacts = contacts.filter(contact => contact.id !== id);
+        this.setState({contacts: contacts});
+
+        /*
+        //for (let item of list) {
+        for (let i = 0; i < list.length; i++) {
+            if (list[i].key === key) {
+                list.splice(i);
+            }
         }
-        this.setState( {
-            contacts: data
-        })}
+        throw new Error("Failed to remove contact! key: " + key);
+        */
+    }
+
+
 
     //<contact fname="Odd" lname="Kristian" />
 /*
 {fname=contact.fname} {lname=contact.lname} />
                     <Text key = {Math.random()}>Name: {contact.fname} {contact.lname}</Text>)
- */
-
-    render() {
 
         {this.state.contacts.map(x =>
             console.log("from map: ",x.fname))}
 
+
+            ***NB!***
+                <FlatList
+                    data={this.state.contacts}
+                    renderItem={({contact}) => {contact}}
+                    />
+                        <Contact key={contact.key} fname={contact.fname} lname={contact.lname}  />)
+ */
+
+    render() {
+
         return (
             <View>
                 <View>
-                {this.state.contacts.map(contact =>
-                    <Contact fname={contact.fname} lname={contact.lname} key={Math.random()} />)
-                }
-                    {console.log(this.state.contacts)}
+                    {this.state.contacts.map(contact => contact)}
                 </View>
-            <View>
-            {}
-            </View>
+
+                <View>
+                {<Button
+                    onPress={() => this.addContact("New","Contact")}
+                    title="Add"
+                    color="#20FF18"
+                    accessibilityLabel="accessibilityLabel er denne teksten"
+                />}
+                </View>
             </View>
         );
     }
