ca99faac705c5d6b15efc1d36cc97735dae28103
Birger <birgerto@stub.ntnu.no>
2018-10-19 14:14:22 +0200

    create drawer navigator #20

--- opg3/App.js
+++ opg3/App.js
@@ -3,18 +3,49 @@ import {StyleSheet, View} from 'react-native';
 import Calendar from './Calendar.js';
 import ContactManager from './Contacts.js';
 import Goals from './Goals.js';
+import { createDrawerNavigator } from 'react-navigation';
 
-export default class App extends React.Component {
+class CalendarScreen extends React.Component {
     render() {
         return (
             <View style={styles.container}>
                 <Calendar />
-                <Goals/>
-                <ContactManager />
             </View>
         );
     }
 }
+
+class ContactsScreen extends React.Component {
+    render() {
+        return (
+            <View style={styles.container, styles.centered}>
+                <ContactManager />
+            </View>
+        )
+    }
+}
+
+class GoalsScreen extends React.Component {
+    render() {
+        return(
+            <View style={styles.container, styles.centered}>
+                <Goals />
+            </View>
+        )
+    }
+}
+
+export default createDrawerNavigator({
+    Calendar: {
+        screen: CalendarScreen,
+    },
+    Contacts: {
+        screen: ContactsScreen,
+    },
+    Goals: {
+        screen: GoalsScreen,
+    },
+});
 
 const styles = StyleSheet.create({
     container: {
